17cd869f263d36eba12ff31192b94697921324e4
Arthur <leroy137.artur@gmail.com>
2020-09-30 18:23:00 +0500

    + основной класс комманды + комманда закрытия окна

---
+++ Shinkuro/Infrastracture/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,21 @@
+﻿using Shinkuro.Infrastracture.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Shinkuro.Infrastracture.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
