2dce785817cb9e984ffcec36e0c8241b17d10bbe
Arthur <leroy137.artur@gmail.com>
2020-09-30 18:03:31 +0500

    + базовый класс команд

---
+++ Shinkuro/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Shinkuro.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shinkuro.ViewModels
+{
+    internal class MainWindowViewModel :ViewModelBase
+    {
+        private String _title;
+
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public String Title
+        {
+            get { return _title; }
+            set
+            {
+                Set(ref _title, value);
+            }
+        }
+
+
+    }
+}
