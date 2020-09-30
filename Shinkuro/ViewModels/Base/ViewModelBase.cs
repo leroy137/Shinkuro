7dc2da7dea18bcc6495186c84285916f8d918683
Arthur <leroy137.artur@gmail.com>
2020-09-30 16:34:23 +0500

    + базовый класс модели представления ^ структура проекта

---
+++ Shinkuro/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Shinkuro.ViewModels.Base
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]String prop = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] String prop=null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(prop);
+            return true;
+        }
+
+    }
+}
