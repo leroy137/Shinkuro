f7dfd8dfa31da6f0a2175fc54fcf13f317b3a11e
Arthur <leroy137.artur@gmail.com>
2020-10-01 21:37:31 +0500

    + сервис фигуры - шаблоны

---
+++ Shinkuro/Models/Interfaces/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shinkuro.Models.Interfaces
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
---
+++ Shinkuro/ViewModels/MainCompetitionViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shinkuro.ViewModels.Base;
+using Shinkuro.Infrastracture.Commands;
+using Shinkuro.Infrastracture.Commands.Base;
+using System.Windows.Input;
+
+namespace Shinkuro.ViewModels
+{
+    /// <summary>
+    /// главная вью модель окна управления текущим соревнованием
+    /// </summary>
+    internal class MainCompetitionViewModel : ViewModelBase
+    {
+
+        #region Команды
+        
+        // закрытие приложения
+        public ICommand CloseApplicationCommand { get; private set; } = new CloseApplicationCommand();
+
+        #endregion
+
+    }
+}
