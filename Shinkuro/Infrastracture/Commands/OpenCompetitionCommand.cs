07658debcc62781f783e69e9543e4842ce947b32
Arthur <leroy137.artur@gmail.com>
2020-10-20 22:06:48 +0500

    группы представление - рефактроинг

---
+++ Shinkuro/Infrastracture/Commands/OpenCompetitionCommand.cs
@@ -0,0 +1,28 @@
+﻿using Shinkuro.Infrastracture.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Shinkuro.Infrastracture.Commands
+{
+    internal class OpenCompetitionCommand : Command
+    {
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Произошла ошибка!!!");
+            }
+        }
+    }
+}
